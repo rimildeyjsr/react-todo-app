d4b2b0225dabfd95934a578299bf2d20bdc21596
Rimil Dey <rimil@springboard.com>
2019-06-09 00:12:09 +0530

    1. added styles using material-ui 2. installed material-ui/icons and material-ui/styles

--- src/components/Todo-Component/Todo.js
+++ src/components/Todo-Component/Todo.js
@@ -1,6 +1,80 @@
 import React, {Component} from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import TextField from '@material-ui/core/TextField';
+import Icon from '@material-ui/core/Icon';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import DoneIcon from '@material-ui/icons/Done';
+import red from '@material-ui/core/colors/red';
+import green from '@material-ui/core/colors/green';
+import blue from '@material-ui/core/colors/blue';
+import Card from '@material-ui/core/Card';
 
-class Todo extends Component {
+
+
+const redColor = red[500];
+const greenColor = green[500];
+const blueColor = blue[500];
+
+const useStyles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  },
+  textField: {
+    marginLeft: '16px',
+    marginRight: '16px',
+    width: 200,
+  },
+  fab: {
+    margin: '25px',
+    width: '40px',
+    height: '40px',
+    color: '#ffffff',
+  },
+  extendedIcon: {
+    marginRight: '16px',
+  },
+  commonFab: {
+    width: '45px',
+    height: '45px',
+    margin: '15px',
+  },
+  editFab: {
+    backgroundColor: blueColor,
+  },
+  deleteFab: {
+    backgroundColor: redColor,
+  },
+  doneFab: {
+    backgroundColor: greenColor,
+  },
+  card: {
+    width: '500px',
+    margin: '20px 0px',
+    padding: '20px',
+  },
+  containerUl: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  fabContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  noteText: {
+    fontSize: '20px',
+  }
+});
+
+class Todo extends React.Component {
   state = {
     edit: false,
     id: null,
@@ -29,6 +103,11 @@ class Todo extends Component {
       date: new Date()
     }
   ]};
+
+  constructor(props)
+  {
+    super(props);
+  }
 
   onSubmitHandle(event) {
     event.preventDefault();
@@ -94,55 +173,82 @@ class Todo extends Component {
 
   renderEditForm() {
     if (this.state.edit) {
+      const { classes } = this.props;
       return (
         <form onSubmit={this.onUpdateHandle.bind(this)}>
-          <input
-            type="text"
+          <TextField
             name="updatedItem"
-            className="item"
             defaultValue={this.state.title}
+            label="Update Item"
+            placeholder="Add Item's new name here"
+            margin="normal"
           />
-          <button className="update-add-item">
-            Update
-          </button>
+          <Fab
+            aria-label="Edit"
+            className={[classes.fab,classes.editFab,classes.commonFab].join(" ")}
+            type='submit'
+          >
+            <EditIcon/>
+          </Fab>
         </form>
       )
     }
   }
 
   render() {
+    const { classes } = this.props;
+
     return (
       <div>
-        {this.renderEditForm()}
         <h1>Minimal To-Do App</h1>
-        <form onSubmit={this.onSubmitHandle.bind(this)}>
-          <input type="text" name="item" className="item" />
-          <button className="btn-add-item">Add</button>
+        <form onSubmit={this.onSubmitHandle.bind(this)} className={classes.container}>
+          <TextField
+            name="item"
+            className={classes.textField}
+            label="Add item"
+            placeholder="Add a new item here"
+            margin="normal"
+          />
+          <Fab color="primary" aria-label="Add" className={classes.fab} type='submit'>
+            <AddIcon />
+          </Fab>
         </form>
-        <ul>
+        <ul className={classes.containerUl}>
           {
             this.state.mockData.map(item => (
-              <li
-                key={item.id}
-                className={ item.done ? 'done' : 'hidden' }
-              >
-                {item.title}
-                <button
-                  onClick={this.onDeleteHandle.bind(this, item.id)}
+              <Card className={classes.card}>
+                <li
+                  key={item.id}
+                  className={ item.done ? 'done' : 'hidden' }
                 >
-                  Delete
-                </button>
-                <button
-                  onClick={this.onEditHandle.bind(this, item.id, item.title)}
-                >
-                  Edit
-                </button>
-                <button
-                  onClick={this.onCompleteHandle.bind(this, item.id)}
-                >
-                  Complete
-                </button>
-              </li>
+                  <p className={classes.noteText}> {item.title}</p>
+                  <div className={classes.fabContainer}>
+                    <Fab
+                      aria-label="Delete"
+                      className={[classes.fab,classes.deleteFab,classes.commonFab].join(" ")}
+                      onClick={this.onDeleteHandle.bind(this, item.id)}
+                    >
+                      <DeleteIcon />
+                    </Fab>
+                    <Fab
+                      aria-label="Edit"
+                      className={[classes.fab,classes.editFab,classes.commonFab].join(" ")}
+                      onClick={this.onEditHandle.bind(this, item.id, item.title)}
+                    >
+                      <EditIcon/>
+                    </Fab>
+                    <Fab
+                      aria-label="Complete"
+                      className={[classes.fab,classes.doneFab,classes.commonFab].join(" ")}
+                      onClick={this.onCompleteHandle.bind(this, item.id)}
+                    >
+                      <DoneIcon/>
+                    </Fab>
+                  </div>
+                </li>
+
+                {this.renderEditForm()}
+              </Card>
             )
             )}
         </ul>
@@ -151,4 +257,4 @@ class Todo extends Component {
   }
 }
 
-export default Todo;
+export default withStyles(useStyles())(Todo);
